f8e40a358dcf1579b488c343be74e708d2944193
Vamshi70752403 <vamshikrishnaa2403@gmail.com>
2025-02-28 18:02:24 +0530

    contact pages done

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,33 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./pages/Home.jsx";
+import Login from './components/Login';
+import Register from './components/Register';
 import ConsultationForm from "./Components/homecomponents/TopConsultants/TopConsultantForm.jsx";
 import BlogCards from "./Components/homecomponents/article/BlogCards.jsx";
 import HomePage from "./Components/homecomponents/article/HomePage.jsx";
 import ViewBlogCard from "./Components/homecomponents/article/ViewBlogCard.jsx";
+import ContactForm from "./pages/Contact.jsx";
 
 
 
 function App() {
   return (
     <>
+       <div className="min-h-screen bg-white">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+         <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         <Route path="/consultation" element={<ConsultationForm />} />
         <Route path="/blogcards" element={<BlogCards/>}/>
         <Route path="/homepage" element={<HomePage/>}/>
         <Route path="/viewblogcard/:id" element={<ViewBlogCard/>}/>
+        <Route path="/contacts" element={<ContactForm/>}/>
       </Routes>
       <Footer />
+        </div>
     </>
   );
 }
