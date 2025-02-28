c894c1d7325d6af12fc7346e4f8fb11382a4ede4
Manohar-M-233 <maddularupasrimanohar2001@gmail.com>
2025-02-28 15:18:19 +0530

    article

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./pages/Home.jsx";
 import ConsultationForm from "./Components/homecomponents/TopConsultants/TopConsultantForm.jsx";
+import BlogCards from "./Components/homecomponents/article/BlogCards.jsx";
+import HomePage from "./Components/homecomponents/article/HomePage.jsx";
+import ViewBlogCard from "./Components/homecomponents/article/ViewBlogCard.jsx";
 
 
 
@@ -13,10 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/consultation" element={<ConsultationForm />} />
-
-      
+        <Route path="/blogcards" element={<BlogCards/>}/>
+        <Route path="/homepage" element={<HomePage/>}/>
+        <Route path="/viewblogcard/:id" element={<ViewBlogCard/>}/>
       </Routes>
-
       <Footer />
     </>
   );
